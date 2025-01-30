53d7d5788c71807dcd97127dd20bdb2e9c0673c4
jiraheta003 <jenniraheta003@gmail.com>
2025-01-29 21:09:20 -0500

    Updated login
    Need help on page showing up on screen

--- app/login.js
+++ app/login.js
@@ -1,9 +1,81 @@
 import React, {useState } from 'react';
-import { TextInput, Button, Alert } from 'react-native';
+import { TextInput, Button, Alert, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
+import { View } from 'react-native';
 
-export const Login = () => {
+const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [authToken, setAuthToken] = useState(null);
+  const router = useRouter();
+  
+  const handleSubmit = async () => {
+
+    const response = await fetch('/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({email, password})
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setAuthToken(data.token);
+      router.push('/dashboard');
+    } else{
+      Alert.alert('Error: Invalid Credentials')
+    }
+  };
+      
+
+  return(
+    <View style = {styles.container}>
+    <Text style = {styles.header}>Login</Text>
+
+
+      <TextInput
+        style = {styles.input}
+        placeholder="username@email.com"
+        value={email}
+        onChangeText={setEmail}   
+      />
+
+      <TextInput
+        style = {styles.input}
+        placeholder="password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+          />
+
+      <Button title="Login" onPress={handleSubmit} />
+    </View>  
+      );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+
+  input: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 14,
+    paddingHorizontal: 10,
+    borderRadius: 5, 
+  }
+});
+
+export default Login;
 
   
